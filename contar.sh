c1582fa9457b8a9a8479482e741223eeb91d0780
valmaldonado <valemaldonado@hotmail.com>
2023-04-14 11:08:38 -0300

    aclaración en archivo contar

--- contar.sh
+++ contar.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 for i in {1..5}
 do
-    #recorre los distintos archivos
+    #recorre los distintos archivos txt generados por lipsum
     cantidadDeLineas=$(cat loremipsum-$i.txt| wc -l)
-    #devuelve sus nombres con la cantidad de lineas
-    echo  loremipsum-$i.txt tiene $cantidadDeLineas lineas
+    
+    #devuelve sus nombres con la cantidad de lineas que existen dentro del archivo
+    echo  loremipsum-$i.txt tiene 
+    
+    $cantidadDeLineas lineas
 done
